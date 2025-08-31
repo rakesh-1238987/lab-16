165a0bd3518a897f8f6bf3ecc34bab6a1e37af75
rakesh-1238987 <rottela.rakesh124031@marwadiuniversity.ac.in>
2025-08-31 23:07:42 +0530

    Add files via upload

---
+++ lab3/p3.py
@@ -0,0 +1,5 @@
+s = input("Enter a string: ")
+if s.isdigit():
+    print("String contains only digits")
+else:
+    print("String does not contain only digits")
---
+++ lab3/p5.py
@@ -0,0 +1,3 @@
+sentence = input("Enter a sentence: ").strip()
+words = sentence.split()
+print("Length of last word:", len(words[-1]))
